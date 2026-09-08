901f8294a2a50bb15bc7d0ca01c7b78b7b33d7ed
Anthony <velurn123@gmail.com>
2026-09-08 17:22:45 -0600

    Initial commit

---
+++ pymez/src/main/java/com/pymez/backend/pymez/PymezApplication.java
@@ -0,0 +1,13 @@
+package com.pymez.backend.pymez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PymezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PymezApplication.class, args);
+	}
+
+}
---
+++ pymez/src/test/java/com/pymez/backend/pymez/PymezApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pymez.backend.pymez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PymezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
