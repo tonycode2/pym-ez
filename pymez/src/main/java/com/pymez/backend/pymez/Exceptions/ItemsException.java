c1dd435ae50c67a05a32bdb20e2b59751fe778a8
Anthony <velurn123@gmail.com>
2026-09-08 20:04:14 -0600

    feat(items): Repository created using JpaRepository interface.

--- pymez/src/main/java/com/pymez/backend/pymez/Exceptions/ItemsException.java
+++ pymez/src/main/java/com/pymez/backend/pymez/Exceptions/ItemsException.java
@@ -6,12 +6,15 @@ public class ItemsException extends RuntimeException{
      * This is an exception that must be used for everyting related to items
      */
     public ItemsException(){}
+
     public ItemsException(String message){
         super(message);
     }
+
     public ItemsException(Throwable cause){
         super(cause);
     }
+    
     public ItemsException(String message, Throwable cause){
         super(message, cause);
     }
