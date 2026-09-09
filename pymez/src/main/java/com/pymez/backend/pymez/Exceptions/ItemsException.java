e70c2d02ecc1c20243031e2b34fef7e3225597f5
Anthony <velurn123@gmail.com>
2026-09-08 19:17:27 -0600

    feat(items): adding custom exception for Items

---
+++ pymez/src/main/java/com/pymez/backend/pymez/Exceptions/ItemsException.java
@@ -0,0 +1,18 @@
+package com.pymez.backend.pymez.Exceptions;
+
+
+public class ItemsException extends RuntimeException{
+    /**
+     * This is an exception that must be used for everyting related to items
+     */
+    public ItemsException(){}
+    public ItemsException(String message){
+        super(message);
+    }
+    public ItemsException(Throwable cause){
+        super(cause);
+    }
+    public ItemsException(String message, Throwable cause){
+        super(message, cause);
+    }
+}
